7ea9c7dc1586013f7e59d0f17e8d73dc8e3a5420
Ayushi Saxena <ayushisaxena@presidio.com>
2025-02-05 17:53:12 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) console.error("Database connection failed:", err);
+  else console.log("Connected to MySQL");
+});
+
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+app.listen(8000, () => console.log("Backend running on port 8000"));
